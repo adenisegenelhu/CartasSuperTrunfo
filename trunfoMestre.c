8075210882b7951f43a51ede345eb9d4b56b145c
adenisegenelhu <adenisegenelhu@gmail.com>
2025-08-08 21:53:19 +0100

    super trunfo

--- trunfoMestre.c
+++ trunfoMestre.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-    int main (){
+int main (){
 
         //Carta 1
         char estado1;
--- trunfoNovato.c
+++ trunfoNovato.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-       int main(){
-       
+int main(){
        //Carta 1
         char estado1;
         char codigo1[4];
